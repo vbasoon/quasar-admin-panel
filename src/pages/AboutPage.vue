61cc7ed21f1fa7501ac9a79b6d984af6a5ae2a1c
Vlad Basoon <vbasoon@gmail.com>
2022-05-14 05:45:05 +0300

    first

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <h1>About</h1>
+    <pre>{{ message }}</pre>
+  </q-page>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  setup() {
+    const message = 'Hello World!';
+    return {
+      message
+    },
+  }
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      { path: "/about", component: () => import("pages/AboutPage.vue") },
+      { path: "/product", component: () => import("pages/ProductPage.vue") },
+      { path: "/contacts", component: () => import("pages/ContactsPage.vue") },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
